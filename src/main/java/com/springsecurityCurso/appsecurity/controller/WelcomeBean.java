cb610058f414753de352918222280fec5c1cfd72
Felipe Jr <felipo_30@hotmail.com>
2024-03-14 21:24:55 -0600

    01-security-basic

---
+++ src/main/java/com/springsecurityCurso/appsecurity/controller/WelcomeBean.java
@@ -0,0 +1,17 @@
+package com.springsecurityCurso.appsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping(path="/welcome")
+public class WelcomeBean {
+
+    @GetMapping
+    public Map<String,String> wecolme(){
+        return Collections.singletonMap("msj","welcome");
+    }
+}
---
+++ src/test/java/com/springsecurityCurso/appsecurity/AppSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurityCurso.appsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
